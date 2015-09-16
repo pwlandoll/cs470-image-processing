f0a4f0878a640cf599b53d3106b2e69012ebfa35
Peter Landoll <pwlandoll@gmail.com>
2015-09-16 15:02:24 -0400

    Testing a url download method, added sample image list

--- plugin/Medical_Image_Processing.py
+++ plugin/Medical_Image_Processing.py
@@ -1,1 +1,11 @@
-from ij import IJ
+#from ij import IJ
+import urllib2
+
+def readURLList(filename):
+	f, images = open(filename, 'r'), []
+	for line in f:
+		images.append(urllib2.urlopen(line))
+	f.close()
+	return images
+
+urls = readURLList("samplelist.txt")
