87ebab314868222189df99740b542fe8dcf0aa19
mbarnewall17 <mbarnewall17@jcu.edu>
2015-09-16 20:41:12 -0400

    File explorer working and split button functionality started
    The file explorer will now open and open any image file selected using
    the bio format plugin. The browse button now works as a split button;
    however the only option at the moment is open image(s)

--- plugin/menu.py
+++ plugin/menu.py
@@ -3,7 +3,12 @@ from javax.swing import JPanel
 from javax.swing import JTextField
 from javax.swing import JButton
 from javax.swing import JFileChooser
-from ij.io import OpenDialog
+from javax.swing import JMenu
+from javax.swing import JMenuBar
+from javax.swing import JMenuItem
+from javax.swing import JPopupMenu
+from java.lang import System
+from javax.swing.filechooser import FileNameExtensionFilter
 from loci.plugins import BF
 
 class Example:
@@ -11,27 +16,68 @@ class Example:
 	# Opens an open dialog box for the user to select a file
 	# Once selected, the file path is added to the textbox
 	def browseForFile(self,event):
+
+		# Creates a file chooser object
 		chooseFile = JFileChooser()
+
+		# Allow for selection of files or directories
 		chooseFile.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES)
-		ret = chooseFile.showDialog(self.frame, "Choose file")
+
+		# Filter results
+		filter = FileNameExtensionFilter("Image Files", ["jpg", "gif"])
+		chooseFile.addChoosableFileFilter(filter)
+		
+		# Show the chooser
+		ret = chooseFile.showDialog(self.textfield, "Choose file")
 		if chooseFile.getSelectedFile() is not None:
 			self.textfield.text = chooseFile.getSelectedFile().getPath()
-			#file = "/Users/curtis/data/0-toucan.png"
-			#imps = BF.openImagePlus(file)
-			#for imp in imps:
-    		#imp.show()
+			if ret == JFileChooser.APPROVE_OPTION:
+				# Open the file using bio formats plugin
+				self.file = chooseFile.getSelectedFile().getPath()
+				imps = BF.openImagePlus(self.file)
+				for imp in imps:
+					imp.show()
+
+	def onExit(self, event):
+		System.exit(0)
 
 	def __init__(self):
-		frame = JFrame("Medical Image Processing")
-		frame.setSize(600, 300)
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+		# Create the menu frame with size of 600x300
+		self.frame = JFrame("Medical Image Processing")
+		self.frame.setSize(600, 300)
+		self.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+
+		# Add a panel to the frame
 		pnl = JPanel()
-		frame.add(pnl)
+		self.frame.add(pnl)
+
+		# Add a textfield to the frame
 		self.textfield = JTextField('',30)
 		pnl.add(self.textfield)
-		copyButton = JButton('Browse',actionPerformed=self.browseForFile)
-		pnl.add(copyButton)
-		frame.setVisible(True)
+
+		# Add a browse button to the frame
+		self.browseButton = JButton('Browse',actionPerformed=self.optionMenuPopup)
+		pnl.add(self.browseButton)
+
+		# Add a menu to the frame
+		menubar = JMenuBar()
+		file = JMenu("File")
+		fileExit = JMenuItem("Exit", None, actionPerformed=self.onExit)
+		fileExit.setToolTipText("Exit application")
+		file.add(fileExit)
+		menubar.add(file)
+		self.frame.setJMenuBar(menubar)
+
+		# Show the frame, done last to show all components
+		self.frame.setVisible(True)
+
+	def optionMenuPopup(self, event):
+		menu = JPopupMenu()
+		singleImage = JMenuItem("Open image(s)", actionPerformed=self.browseForFile)
+		singleImage.setToolTipText("Browse for any number of images to open")
+		menu.add(singleImage)
+		menu.show(self.browseButton, self.browseButton.getWidth(), 0)
+		
 
 if __name__ == '__main__':
 	#start things off.
