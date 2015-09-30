b9610c4bdcb13d83f89b4ee3613258704bcb55c9
Peter Landoll <peter@Peters-MacBook-Pro.local>
2015-09-30 11:50:59 -0400

    Added a small function for calling R, just for an example

--- plugin/Medical_Image_Processing.py
+++ plugin/Medical_Image_Processing.py
@@ -1,4 +1,5 @@
 from ij import IJ
+from pyper import *
 import os
 
 def readURLList(filename):
@@ -12,3 +13,9 @@ filename = "C:\Users\Peter\Documents\GitHub\cs470-image-processing\plugin\sample
 
 for image in readURLList(filename):
 	image.show()
+
+def runRScript():
+	r = R()
+	output = r('a <- 5; print(a)')
+	return output
+
