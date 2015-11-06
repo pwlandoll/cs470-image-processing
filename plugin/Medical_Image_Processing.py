cbf448deaba25d1e6947a8d6a59064e937e88939
Peter Landoll <pwlandoll@gmail.com>
2015-11-06 09:56:49 -0500

    Combined checkIfPathSet and rScriptSearch methods into new setRPath method

--- plugin/Medical_Image_Processing.py
+++ plugin/Medical_Image_Processing.py
@@ -217,9 +217,12 @@ class ImageProcessorMenu:
 		# Show the frame, done last to show all components
 		self.frame.setResizable(False)
 		self.frame.setVisible(True)
-		self.checkIfPathSet()
+		self.setRPath()
 
-	def checkIfPathSet(self):
+	def setRPath(self):
+		# TODO: Replace with new text file storage method
+		# 	In general: Try to read in the path from the text file
+		# 	If that comes up with nothing or the file doesn't exist, proceed
 		pluginDir = IJ.getDir("plugins") + "\Medical_Image"
 		# Create the file to house the path
 		file = File(pluginDir + "\Medical_Image_Processing.txt")
@@ -229,7 +232,23 @@ class ImageProcessorMenu:
 						On OS X, Rscript is usually found in /usr/local/bin/.\
 						On Linux, Rscript is usually found in /usr/bin."
 			JOptionPane.showMessageDialog(self.frame, message)
-			self.rScriptSearch()
+			# Look for the Rscript command. First, try known locations for OS X, Linux, and Windows
+			osxdir, linuxdir, windowsdir = "/usr/local/bin/Rscript", "/usr/bin/Rscript", "C:\\Program Files\\R" 
+			if os.path.exists(osxdir):
+				self.rcommand = osxdir
+			elif os.path.exists(linuxdir):
+				self.rcommand = linuxdir
+			elif os.path.exists(windowsdir):
+				self.rcommand = next(os.walk(windowsdir))[1][-1]
+			# If none of those work
+			if not self.rcommand:
+				chooseFile = JFileChooser()
+				chooseFile.setFileSelectionMode(JFileChooser.FILES_ONLY)
+				# TODO: Verify that the selected file is Rscript
+				if chooseFile.showDialog(self.frame, "Select") is not None:
+					self.rcommand = chooseFile.getSelectedFile()
+				#JOptionPane.showMessageDialog(self.frame, self.rcommand)
+				#self.saveToFile()
 
 	#Enables/Disables the file extension textfield based on the user's selected delimiter
 	def setExtensionTextfieldEnabled(selectedDelimiter):
@@ -864,27 +883,6 @@ class ImageProcessorMenu:
 					except IOException:
 						print "IO Exception"
 					break
-
-	### The following two methods are intended to replace rSearch().
-	# Looks for RScript command
-	def rScriptSearch(self):
-		# First, try known locations for OS X, Linux, and Windows
-		osxdir, linuxdir, windowsdir = "/usr/local/bin/Rscript", "/usr/bin/Rscript", "C:\\Program Files\\R" 
-		if os.path.exists(osxdir):
-			self.rcommand = osxdir
-		elif os.path.exists(linuxdir):
-			self.rcommand = linuxdir
-		elif os.path.exists(windowsdir):
-			self.rcommand = next(os.walk(windowsdir))[1][-1]
-		# If none of those work
-		if not self.rcommand:
-			chooseFile = JFileChooser()
-			chooseFile.setFileSelectionMode(JFileChooser.FILES_ONLY)
-			# TODO: Verify that the selected file is Rscript
-			if chooseFile.showDialog(self.frame, "Select") is not None:
-				self.rcommand = chooseFile.getSelectedFile()
-			#JOptionPane.showMessageDialog(self.frame, self.rcommand)
-			#self.saveToFile()
 
 	# Funtion to open the text file where data is saved
 	# TODO: replace with better method
